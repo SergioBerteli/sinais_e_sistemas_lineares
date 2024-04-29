591c2a862e7fa8a4b9e1a7c8a0337314ba56d1ec
SergioBerteli <sergio@univali.br>
2024-04-29 18:46:09 -0300

    Adição do plot de fase e remoção de modulo de plot

--- m2/transformada_fourier/transf_fourier.py
+++ m2/transformada_fourier/transf_fourier.py
@@ -1,5 +1,4 @@
-from numpy import where, linspace,  e, fft, pi, array
-from plot import add_subplot, imprimir_grafico
+from numpy import where, linspace,  e, fft, abs as absnp, angle
 import matplotlib.pyplot as plt
 
 x_inicio = -3
@@ -14,22 +13,33 @@ fou_transf = fft.fft(sinal) * (t[1] - t[0])
 
 f = fft.fftfreq(x_num, t[1] - t[0])
 
-module = abs(fou_transf)
+module = absnp(fou_transf)
+phase = angle(fou_transf)
+
 if __name__ == "__main__":
     #add_subplot(t, sinal, "sinal em função do tempo", 1)
     #add_subplot(f, module, "sinal no dominio da frequencia", 2)
     #imprimir_grafico()
-    plt.figure(figsize=(10, 8))
-    plt.subplot(2, 1, 1)
-    plt.plot(f, module)
-    plt.title("sinal no dominio da frequencia")
-    plt.xlabel('Frequencia (Hz)')
-    plt.ylabel('Magnitude')
-    plt.grid(True)
-    plt.subplot(2, 1, 2)
+    # plt.figure(figsize=(10, 8))
+    plt.subplot(3, 1, 1)
     plt.plot(t, sinal)
-    plt.title("sinal no dominio do tempo")
+    plt.title("Modulo do sinal no dominio da frequencia")
     plt.xlabel('Tempo (t)')
     plt.ylabel('f(t)')
     plt.grid(True)
+    plt.subplot(3, 1, 2)
+    plt.plot(f, module)
+    plt.title("Sinal no dominio da frequencia")
+    plt.xlabel('Frequencia (Hz)')
+    plt.ylabel('Magnitude')
+    plt.grid(True)
+    plt.subplot(3, 1, 3)
+    plt.plot(f, phase)
+    plt.title("Fase do sinal no dominio da frequencia")
+    plt.xlabel('Frequencia (Hz)')
+    plt.ylabel('Fase')
+    plt.grid(True)
+    
+    plt.subplots_adjust(hspace=0.5)
+
     plt.show()
